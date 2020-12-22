581e787a35090ef2efc3f556a38a10277181b8c4
Seresma <sergiem10@hotmail.com>
2020-12-22 14:59:21 +0100

    UPDATED Ventana login
    He remodelado y cambiado cosas de la ventana login.

--- src/main/java/um/tds/appMusic/gui/Login.java
+++ src/main/java/um/tds/appMusic/gui/Login.java
@@ -8,22 +8,26 @@ import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.GridBagLayout;
+
 import javax.swing.JTextField;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.SwingConstants;
 import javax.swing.JPasswordField;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.FlowLayout;
-import javax.swing.border.MatteBorder;
-import java.awt.SystemColor;
-import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.BevelBorder;
+
+import java.awt.Component;
+
 
 public class Login {
 
-	private JFrame frame;
+	private JFrame LoginFrame;
 	private JTextField userField;
 	private JPasswordField passwordField;
 
@@ -35,14 +39,15 @@ public class Login {
 			public void run() {
 				try {
 					Login window = new Login();
-					window.frame.setVisible(true);
+					window.LoginFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-
+	
+	
 	/**
 	 * Create the application.
 	 */
@@ -54,90 +59,133 @@ public class Login {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 466, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel titlePanel = new JPanel();
-		titlePanel.setBorder(new MatteBorder(0, 0, 2, 0, (Color) SystemColor.activeCaption));
-		frame.getContentPane().add(titlePanel, BorderLayout.NORTH);
-		
-		JLabel titleLabel = new JLabel("AppMusic");
-		titleLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-		titlePanel.add(titleLabel);
+		LoginFrame = new JFrame();
+		LoginFrame.setResizable(false);
+		LoginFrame.setTitle("AppMusic");
+		LoginFrame.setBounds(100, 100, 480, 300);
+		LoginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ImageIcon iconoAppMusic = new ImageIcon("icons/iconoAppMusic.png");
+		LoginFrame.setIconImage(iconoAppMusic.getImage());
+
 		
 		JPanel loginPanel = new JPanel();
+		loginPanel.setBackground(Color.LIGHT_GRAY);
 		loginPanel.setBorder(null);
-		frame.getContentPane().add(loginPanel, BorderLayout.CENTER);
+		LoginFrame.getContentPane().add(loginPanel, BorderLayout.CENTER);
 		GridBagLayout gbl_loginPanel = new GridBagLayout();
 		gbl_loginPanel.columnWidths = new int[] {200};
-		gbl_loginPanel.rowHeights = new int[] {30, 30, 30, 30, 30};
+		gbl_loginPanel.rowHeights = new int[] {0, 30, 30, 30, 30, 0, 30};
 		gbl_loginPanel.columnWeights = new double[]{0.0};
-		gbl_loginPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0};
+		gbl_loginPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 		loginPanel.setLayout(gbl_loginPanel);
 		
+		JLabel titleLabel = new JLabel("AppMusic");
+		titleLabel.setForeground(new Color(178, 34, 34));
+		titleLabel.setFont(new Font("Tahoma", Font.BOLD, 19));
+		GridBagConstraints gbc_titleLabel = new GridBagConstraints();
+		gbc_titleLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_titleLabel.gridx = 0;
+		gbc_titleLabel.gridy = 0;
+		loginPanel.add(titleLabel, gbc_titleLabel);
+		
 		JLabel userLabel = new JLabel("Usuario:");
+		userLabel.setForeground(Color.BLACK);
 		userLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		userLabel.setVerticalAlignment(SwingConstants.BOTTOM);
 		GridBagConstraints gbc_userLabel = new GridBagConstraints();
 		gbc_userLabel.fill = GridBagConstraints.BOTH;
 		gbc_userLabel.insets = new Insets(0, 0, 5, 0);
 		gbc_userLabel.gridx = 0;
-		gbc_userLabel.gridy = 0;
+		gbc_userLabel.gridy = 1;
 		loginPanel.add(userLabel, gbc_userLabel);
 		
 		userField = new JTextField();
+		userField.setBackground(new Color(245, 245, 245));
+		userField.setMargin(new Insets(2, 10, 2, 2));
+		userField.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		userField.setForeground(Color.BLACK);
 		GridBagConstraints gbc_userField = new GridBagConstraints();
 		gbc_userField.fill = GridBagConstraints.BOTH;
 		gbc_userField.insets = new Insets(0, 0, 5, 0);
 		gbc_userField.gridx = 0;
-		gbc_userField.gridy = 1;
+		gbc_userField.gridy = 2;
 		loginPanel.add(userField, gbc_userField);
 		userField.setColumns(10);
 		
 		JLabel passwordLabel = new JLabel("Contrase\u00F1a:");
+		passwordLabel.setForeground(Color.BLACK);
 		passwordLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		passwordLabel.setVerticalAlignment(SwingConstants.BOTTOM);
 		GridBagConstraints gbc_passwordLabel = new GridBagConstraints();
 		gbc_passwordLabel.fill = GridBagConstraints.BOTH;
 		gbc_passwordLabel.insets = new Insets(0, 0, 5, 0);
 		gbc_passwordLabel.gridx = 0;
-		gbc_passwordLabel.gridy = 2;
+		gbc_passwordLabel.gridy = 3;
 		loginPanel.add(passwordLabel, gbc_passwordLabel);
 		
 		passwordField = new JPasswordField();
+		passwordField.setBackground(new Color(245, 245, 245));
+		passwordField.setFont(new Font("Tahoma", Font.BOLD, 13));
+		passwordField.setMargin(new Insets(2, 10, 2, 2));
 		GridBagConstraints gbc_passwordField = new GridBagConstraints();
 		gbc_passwordField.fill = GridBagConstraints.BOTH;
 		gbc_passwordField.insets = new Insets(0, 0, 5, 0);
 		gbc_passwordField.gridx = 0;
-		gbc_passwordField.gridy = 3;
+		gbc_passwordField.gridy = 4;
 		loginPanel.add(passwordField, gbc_passwordField);
 		
-		JButton loginButton = new JButton("Iniciar Sesi\u00F3n");
-		loginButton.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		JButton loginButton = new JButton("Iniciar sesión");
+		loginButton.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+		loginButton.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		loginButton.setBackground(new Color(178, 34, 34));
+		loginButton.setForeground(new Color(255, 255, 255));
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+		JLabel SplitLabel = new JLabel("label de separación");
+		SplitLabel.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		SplitLabel.setForeground(Color.LIGHT_GRAY);
+		GridBagConstraints gbc_SplitLabel = new GridBagConstraints();
+		gbc_SplitLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_SplitLabel.gridx = 0;
+		gbc_SplitLabel.gridy = 5;
+		loginPanel.add(SplitLabel, gbc_SplitLabel);
+		loginButton.setFont(new Font("Tahoma", Font.BOLD, 13));
 		GridBagConstraints gbc_loginButton = new GridBagConstraints();
 		gbc_loginButton.fill = GridBagConstraints.BOTH;
 		gbc_loginButton.gridx = 0;
-		gbc_loginButton.gridy = 4;
+		gbc_loginButton.gridy = 6;
 		loginPanel.add(loginButton, gbc_loginButton);
 		
 		JPanel registerPanel = new JPanel();
-		frame.getContentPane().add(registerPanel, BorderLayout.SOUTH);
+		registerPanel.setBackground(Color.LIGHT_GRAY);
+		LoginFrame.getContentPane().add(registerPanel, BorderLayout.SOUTH);
 		registerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel registerLabel = new JLabel("\u00BFNo tienes cuenta?");
+		JLabel registerLabel = new JLabel("<html>¿<b>No</b> tienes cuenta?</html>");
+		registerLabel.setForeground(Color.BLACK);
 		registerLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		registerLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 		registerLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		registerPanel.add(registerLabel);
 		
 		JLabel arrowLabel = new JLabel("---->");
+		arrowLabel.setForeground(new Color(178, 34, 34));
 		registerPanel.add(arrowLabel);
-		arrowLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		arrowLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
 		
 		JButton registerButton = new JButton("Reg\u00EDstrate");
+		registerButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		registerButton.setBackground(Color.DARK_GRAY);
+		registerButton.setForeground(Color.WHITE);
 		registerPanel.add(registerButton);
-		registerButton.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		registerButton.setFont(new Font("Tahoma", Font.BOLD, 13));
+		
+
 	}
 
 }
